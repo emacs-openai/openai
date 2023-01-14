3309eb3a9ba3f6035d2cb5eac3902c975021db2b
Jen-Chieh <jcs090218@gmail.com>
2023-01-14 16:35:57 +0800

    Add src

---
+++ chatgpt.el
@@ -0,0 +1,66 @@
+;;; chatgpt.el --- API for interacting with ChatGPT  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/jcs090218/ChatGPT.el
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: comm gpt
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; API for interacting with ChatGPT
+;;
+
+;;; Code:
+
+(require 'request)
+
+(defgroup chatgpt nil
+  "Fuzzy matching for `company-mode'."
+  :prefix "chatgpt-"
+  :group 'comm
+  :link '(url-link :tag "Repository" "https://github.com/jcs090218/ChatGPT.el"))
+
+(defcustom chatgpt-key ""
+  "Generated API key."
+  :type 'list
+  :group 'chatgpt)
+
+;;;###autoload
+(defun chatgpt-send (query)
+  ""
+  (let ((url-request-method "POST")
+        (url-request-extra-headers
+         `(("Content-Type"  . "application/json")
+           ("Authorization" . ,(concat "Bearer " chatgpt-key))))
+        (url-request-data
+         (json-encode`(("model"      . "text-davinci-003")
+                       ("prompt"     . ,query)
+                       ("max_tokens" . 4000)
+                       ("temperature" . 1.0)))))
+    (with-current-buffer (url-retrieve-synchronously "https://api.openai.com/v1/completions")
+      (set-buffer-multibyte t)
+      (goto-char url-http-end-of-headers)
+      (prog1 (let ((json-array-type 'list)) (json-read))
+        (kill-buffer)))))
+
+(provide 'chatgpt)
+;;; chatgpt.el ends here

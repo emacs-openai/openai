1dd0beee4aa1a08e9fb584e39a36108b70402037
Cor Zuurmond <jczuurmond@protonmail.com>
2023-04-07 14:28:54 +0200

    Update chat docs

--- openai-chat.el
+++ openai-chat.el
@@ -54,7 +54,7 @@
 Arguments MESSAGES and CALLBACK are required for this type of request.  MESSAGES
 is the conversation data.  CALLBACK is the execuation after request is made.
 
-Arguments CONTENT-TYPE, KEY, ORG-ID and USER are global options; however, you
+Arguments CONTENT-TYPE, KEY, ORG-ID, USER and BASE-URL are global options; however, you
 can overwrite the value by passing it in.
 
 The rest of the arugments are optional, please see OpenAI API reference page
